df93f55a73241f97faea1eb9b108e99da0458bca
orengame <96296894+orengame@users.noreply.github.com>
2021-12-17 15:11:57 +0200

    Sent to HACKEN.IO for AUDIT

--- OREN Token.sol
+++ OREN Token.sol
@@ -1,5 +1,5 @@
 /**
- *Submitted for verification at BscScan.com on 2021-12-07
+ *Submitted for verification at BscScan.com on 2021-12-07.
 */
 
 // File: @openzeppelin/contracts/token/BEP20/IBEP20.sol
